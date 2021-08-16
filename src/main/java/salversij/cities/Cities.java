eea2e5198a7a6f1fadab7dfca406f81a43a30407
sal-versij <salvoluca1212000@gmail.com>
2021-08-16 14:32:39 +0200

    First Commit

---
+++ src/main/java/salversij/cities/Cities.java
@@ -0,0 +1,10 @@
+package salversij.cities;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cities implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/salversij/cities/client/CitiesClient.java
@@ -0,0 +1,13 @@
+package salversij.cities.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
